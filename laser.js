7a6301c7e3f568779163d1c018f3c0909dd6635c
KTheXIII <24420859+KTheXIII@users.noreply.github.com>
2018-11-12 15:44:12 +0100

    Removed laser when offscreen

--- laser.js
+++ laser.js
@@ -27,4 +27,14 @@ function Laser(sPos, angle){
         }
     }
 
+    this.offscreen = function(){
+        if(this.pos.x  > width || this.pos.x < 0){
+            return true;
+        }
+        if(this.pos.y  > height || this.pos.y < 0){
+            return true;
+        }
+        return false;
+    }
+
 }
