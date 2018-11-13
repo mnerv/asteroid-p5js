b0adb6acdf258d0efe3702c309c4cc2b660958fc
KTheXIII <24420859+KTheXIII@users.noreply.github.com>
2018-11-13 12:17:54 +0100

    Fix the hitbox
    The hitbox before for the ship was wrong. It was only checking the distance between the ships radius and the asteroids origin position.

--- ship.js
+++ ship.js
@@ -1,87 +1,92 @@
-function Ship(){
-	this.pos = createVector(width/2, height/2);
+function Ship() {
+	this.pos = createVector(width / 2, height / 2);
 	this.heading = 0
 	this.r = 10
 	this.rotation = 0;
-	this.velocity = createVector(0,0);
+	this.velocity = createVector(0, 0);
 	this.isThrust = false;
 
 	this.friction = 0.97;
 
-	this.update = function(){
+	this.update = function () {
 		this.pos.add(this.velocity);
 		this.velocity.mult(this.friction);
 
-		if(this.isThrust)
+		if (this.isThrust)
 			this.thrust();
 
 		this.edges();
 		this.turn();
 		this.render();
-	}	
+	}
 
-	this.render = function(){
-        push();
+	this.render = function () {
+		push();
 		translate(this.pos.x, this.pos.y);
-		rotate(this.heading + PI/2);
-        // noFill();
-        fill(0);
+		rotate(this.heading + PI / 2);
+		// noFill();
+		fill(0);
 		stroke(255);
 		triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
 
-		stroke(0, 255*0.7, 255*0.7);
+		stroke(0, 255 * 0.7, 255 * 0.7);
 		noFill();
-        beginShape();
-        for (let i = 0; i < 10; i++) {
-            let angle = map(i, 0, 10, 0, TWO_PI);
-            let r = this.r;
-            // Polar to Cartesian coordinate system
-            let x = r * cos(angle);
-            let y = r * sin(angle);
-            vertex(x, y);
-        }
-        endShape(CLOSE);
+		beginShape();
+		for (let i = 0; i < 10; i++) {
+			let angle = map(i, 0, 10, 0, TWO_PI);
+			let r = this.r;
+			// Polar to Cartesian coordinate system
+			let x = r * cos(angle);
+			let y = r * sin(angle);
+			vertex(x, y);
+		}
+		endShape(CLOSE);
 
 		pop();
 	};
 
-	this.edges = function(){
-		if(this.pos.x > width + this.r){
+	this.edges = function () {
+		if (this.pos.x > width + this.r) {
 			this.pos.x = -this.r;
-		} else if(this.pos.x < -this.r){
+		} else if (this.pos.x < -this.r) {
 			this.pos.x = width + this.r;
 		}
 
-		if(this.pos.y > height + this.r){
+		if (this.pos.y > height + this.r) {
 			this.pos.y = -this.r;
-		} else if(this.pos.y < -this.r){
+		} else if (this.pos.y < -this.r) {
 			this.pos.y = height + this.r;
 		}
 	}
 
-	this.hits = function(asteroid){
+	this.hits = function (asteroid) {
 		let d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
-		if(d < this.r){
+		if (d < this.r + asteroid.r) {
 			return true;
 		} else {
 			return false
 		}
 	}
 
-	this.setRotation = function(angle){
+	this.setRotation = function (angle) {
 		this.rotation = angle;
 	}
 
-	this.thrusting = function(tof){
+	this.thrusting = function (tof) {
 		this.isThrust = tof;
 	}
 
-	this.turn = function(){
+	this.turn = function () {
 		this.heading += this.rotation;
 	}
 
-	this.thrust = function(){
-		let force = p5.Vector.fromAngle(this.heading);
+	this.thrust = function () {
+		let force;
+		if (this.rotation == 0) {
+			force = p5.Vector.fromAngle(this.heading);
+		} else {
+			force = p5.Vector.fromAngle(this.heading-this.rotation);
+		}
 		force.mult(0.5);
 		// console.log(Math.pow(force.x, 2) + Math.pow(force.y, 2));
 		this.velocity.add(force);
